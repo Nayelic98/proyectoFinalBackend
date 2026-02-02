259ee869e91cfd60219555b37516316304d3774b
Nayelic98 <cajamarcanaye@gmail.com>
2026-02-02 17:21:17 -0500

    cambio 10

--- src/main/java/ec/edu/ups/icc/proyectofinal/user/repository/SolicitudPostulacionRepository.java
+++ src/main/java/ec/edu/ups/icc/proyectofinal/user/repository/SolicitudPostulacionRepository.java
@@ -3,6 +3,9 @@ package ec.edu.ups.icc.proyectofinal.user.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import ec.edu.ups.icc.proyectofinal.user.models.SolicitudPostulacionEntity;
@@ -12,7 +15,10 @@ import jakarta.transaction.Transactional;
 public interface SolicitudPostulacionRepository extends JpaRepository<SolicitudPostulacionEntity, Long> {
     Optional<SolicitudPostulacionEntity> findByEmail(String email);
         boolean existsByEmail(String email);
-        @Transactional
-    void deleteByContacto(String contacto);
+        @Modifying // Necesario para DELETE o UPDATE
+    @Transactional
+    @Query("DELETE FROM SolicitudPostulacionEntity s WHERE s.contacto = :contacto") 
+    void eliminarPorContacto(@Param("contacto") String contacto);
+
 
 }
